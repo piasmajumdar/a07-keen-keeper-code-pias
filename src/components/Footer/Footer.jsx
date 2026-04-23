9c7054344f5c240fb4a9ac96021d7b24e6bee728
piasmajumdar <piasmajumdar@gmail.com>
2026-04-24 02:55:15 +0530

    responsive navbar with hamburger icon in smaller devixe

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import { RiInstagramFill } from 'react-icons/ri';
 const Footer = () => {
     return (
         <div className='bg-[#244D3F] pt-20 pb-8 text-center'>
-            <div className='w-7/10 mx-auto'>
+            <div className='w-11/12 max-w-[1110px] mx-auto space-y-10'>
                 <div className='space-y-4'>
                     <h2 className='text-5xl text-white font-bold'>KeenKeeper</h2>
                     <p className='text-white/80'>Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
@@ -17,7 +17,7 @@ const Footer = () => {
                         <button className='w-10 h-10 rounded-full bg-white text-[#101727] flex justify-center items-center cursor-pointer'><a href='/'><FaXTwitter className='text-2xl' /></a> </button>
                     </div>
                 </div>
-                <div className='text-white/50 flex justify-between'>
+                <div className='text-white/50 flex justify-between pt-8'>
                     <div>© 2026 KeenKeeper. All rights reserved.</div>
                     <div className='flex gap-4'>
                         <a className='cursor-pointer'>Privacy Policy</a>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,16 +6,38 @@ import { GoGraph } from "react-icons/go";
 
 
 const Navbar = () => {
+    const links = <>
+
+        <NavLink to={'/'} className="btn"><RiHome2Line /> Home</NavLink>
+        <NavLink to={'/timeline'} className="btn"><IoTimeOutline /> Timeline</NavLink>
+        <NavLink to={'/stats'} className="btn"><GoGraph /> Stats</NavLink>
+
+    </>
     return (
         <div className="bg-base-100 shadow-sm">
-            <div className="navbar w-11/12 mx-auto">
-                <div className="flex-1">
+            <div className="navbar justify-between w-11/12 mx-auto">
+                <div className="navbar-start">
+
                     <a className="text-2xl"><span className="text-[#1F2937] font-extrabold">Keen</span><span className="text-[#244D3F] font-semibold">Keeper</span></a>
                 </div>
-                <div className="flex gap-2">
-                    <NavLink to={'/'} className="btn"><RiHome2Line/> Home</NavLink>
-                    <NavLink to={'/timeline'} className="btn"><IoTimeOutline/> Timeline</NavLink>
-                    <NavLink to={'/stats'} className="btn"><GoGraph/> Stats</NavLink>
+                <div className="navbar-center hidden sm:flex">
+                    <ul className="menu menu-horizontal px-1 space-x-4">
+                        {links}
+                    </ul>
+                </div>
+                <div className="dropdown dropdown-end absolute right-0">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+
+                    <ul
+                        tabIndex={-1}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow"
+                    >
+                        {links}
+                    </ul>
                 </div>
             </div>
         </div>
