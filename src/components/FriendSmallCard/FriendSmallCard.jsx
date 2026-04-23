c441388750e07c9595e518f824942afc31952912
piasmajumdar <piasmajumdar@gmail.com>
2026-04-24 04:33:46 +0530

    friends section completed with show all friedns to ui

---
+++ src/components/FriendSmallCard/FriendSmallCard.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const FriendSmallCard = ({ friend }) => {
+    // console.log(friend)
+
+    return (
+        <div>
+            <div className="card bg-base-100 shadow-sm">
+                <figure className='pt-6'>
+                    <img
+                        className='w-20 h-20 rounded-full'
+                        src={friend.picture}
+                        alt="Shoes" />
+                </figure>
+                <div className="card-body text-center">
+                    <h2 className="font-semibold text-xl text-[#1F2937]">{friend.name}</h2>
+                    <h3 className='text-[12px] text-[#64748B]'>{friend.days_since_contact}d ago</h3>
+                    <div className='flex justify-center gap-2 text-[12px] font-medium text-[#244D3F]'>
+                        {
+                            friend.tags.map((tag, ind) => <div key={ind} className='px-2 py-2 rounded-full bg-green-200'>{tag.toUpperCase()}</div>)
+                        }
+                    </div>
+                    <div className='flex justify-center gap-2 text-[12px] font-medium text-white'>
+                        {
+                            friend.status === 'overdue' ?
+                                <div className='px-2 py-2 rounded-full bg-[#EF4444]'>Overdue</div>
+                                : friend.status === 'almost due' ?
+                                    <div className='px-2 py-2 rounded-full bg-[#EFAD44]'>Almost Due</div>
+                                    : <div className='px-2 py-2 rounded-full bg-[#244D3F]'>On-Track</div>
+                        }
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default FriendSmallCard;
--- src/components/Homepage/Friends.jsx
+++ src/components/Homepage/Friends.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import useFriendsData from '../../hooks/useFriendsData';
 import Loader from '../ui/Loader/Loader';
+import FriendSmallCard from '../FriendSmallCard/FriendSmallCard';
 
 const Friends = () => {
     const { friends, loading } = useFriendsData();
-    console.log(friends);
+    // console.log(friends);
     return (
         <div className='pt-10 pb-20 border-t border-t-[#E9E9E9]'>
             <h2 className='font-semibold text-2xl text-[#1F2937] mb-4'>Your Friends</h2>
             {
-                loading ? (<Loader></Loader>) : (<div>Loaded...</div>)
+                loading ? (<Loader></Loader>) : (
+                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+                        {
+                            friends.map(friend => {
+                                return <FriendSmallCard key={friend.id} friend={friend}></FriendSmallCard>
+                            })
+                        }
+                    </div>
+                )
             }
         </div>
 
